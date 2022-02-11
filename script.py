cf2898eb6216bc6c27e6b5deda6c6f452984d082
qwjyh <urataw421@gmail.com>
2022-02-11 13:55:20 +0900

    first plot

--- script.py
+++ script.py
@@ -1,5 +1,9 @@
 import re
 from bs4 import BeautifulSoup
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+
 
 # soup作成
 soup = BeautifulSoup(open('battery-report.html', encoding="utf-8"), "lxml")
@@ -9,15 +13,6 @@ table = soup.find("h2", text=re.compile("Battery capacity history")).next_siblin
 
 # 保存先のリスト
 mat = []
-
-# thead
-# r = [] # 保存先のrow
-# thead = table.find('thead')
-# # print(thead)
-# tds = thead.tr.find_all('td')
-# for td in tds:
-#   r.append(td.get_text(strip = True))
-# mat.append(r) # matにtheadを追加
 
 # td(dateTime), td(mw(capacity)), td(mw(design))
 trs = table.find_all('tr')
@@ -29,4 +24,28 @@ for tr in trs:
 
 # リストの転置
 mat_T = [list(x) for x in zip(*mat)]
-print(mat_T)
+# print(mat_T)
+
+# トリム
+dates = []
+for i in range(1, len(mat_T[0])):
+  dates.append(re.search('\d+-\d+-\d+', mat_T[0][i]).group())
+charge = []
+for i in range(1, len(mat_T[1])):
+  charge.append(int("".join(re.findall('\d', mat_T[1][i]))))
+design = []
+for i in range(1, len(mat_T[2])):
+  design.append(int("".join(re.findall('\d', mat_T[2][i]))))
+print(design)
+
+# dataframeに変換
+df = pd.DataFrame(
+  {mat_T[0][0] : dates,
+   mat_T[1][0] : charge,
+   mat_T[2][0] : design}
+)
+
+print(df)
+
+plt.plot(df.iloc[:, 0], df.iloc[:, 1])
+plt.show()
